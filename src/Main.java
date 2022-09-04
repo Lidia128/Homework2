4f0bb61e27b020fefe64c4f5bfd777cfd7d02b07
Лидия Дворкина <lideya832@mail.ru>
2022-09-04 16:03:07 +0300

    Домашнее задание № 2

---
+++ src/Main.java
@@ -0,0 +1,99 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args){
+
+    //Задача № 1
+        System.out.println("Задача № 1");
+        System.out.println();
+    var dog=8.0;
+        System.out.println(dog);
+    var cat=3.6;
+        System.out.println(cat);
+    var paper=763789;
+        System.out.println(paper);
+        System.out.println();
+    //Задача № 2
+        System.out.println("Задача № 2");
+        System.out.println();
+    dog=dog+4;
+        System.out.println(dog);
+    cat=cat+4;
+        System.out.println(cat);
+    paper=paper+4;
+        System.out.println(paper);
+        System.out.println();
+    //Задача № 3
+        System.out.println("Задача № 3");
+        System.out.println();
+    dog=dog-3.5;
+        System.out.println(dog);
+    cat=cat-1.6;
+        System.out.println(cat);
+    paper=paper-7639;
+        System.out.println(paper);
+        System.out.println();
+    //Задача № 4
+        System.out.println("Задача № 4");
+        System.out.println();
+    var friend=19;
+        System.out.println(friend);
+    friend=19+2;
+        System.out.println(friend);
+    friend=friend/7;
+        System.out.println(friend);
+        System.out.println();
+    //Задача № 5
+        System.out.println("Задача № 5");
+        System.out.println();
+    var frog=3.5;
+        System.out.println(frog);
+    frog=frog*10;
+        System.out.println(frog);
+    frog=frog/3.5;
+        System.out.println(frog);
+        System.out.println();
+    //Задача № 6
+        System.out.println("Задача № 6");
+        System.out.println();
+    var boxerFerst=78.2;
+        System.out.println("Вес певого боксера " +boxerFerst);
+    var boxerSecond=82.7;
+        System.out.println("Вес второго боксера " +boxerSecond);
+    var totelWeight=boxerFerst+boxerSecond;
+        System.out.println("Общий вес двух боксеров " +totelWeight);
+    var differenceWeight=boxerSecond-boxerFerst;
+        System.out.println("Разница в весе " +differenceWeight);
+        System.out.println();
+    //Задача № 7
+        System.out.println("Задача № 7");
+        System.out.println();
+    var ferstKind=boxerSecond-boxerFerst;
+        System.out.println("Разница в весе " +ferstKind);
+    var secondKind=(boxerSecond)%boxerFerst;
+        System.out.println("Разница в всесе другой способ " +secondKind);
+        System.out.println();
+    //Задача № 8
+        System.out.println("Задача № 8");
+        System.out.println();
+    var totalHour=640;
+    var workHouer=8;
+    var peopleWork=totalHour/workHouer;
+        System.out.println("Всего работников в компании – " + peopleWork + " человек.");
+    var newPeople=peopleWork+94;
+    var newHouer=newPeople*workHouer;
+        System.out.println("Если в компании работает " + newPeople + " человека, то всего " +newHouer+
+                " часов работы может быть поделено между сотрудниками.");
+        System.out.println();
+
+
+
+
+
+
+
+
+
+    }}
+
+
